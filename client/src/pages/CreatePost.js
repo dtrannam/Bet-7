f62e7fef89a8d358aa563a097a3d5c8640a90e6d
David Trannam <trannamd@oregonstate.edu>
2023-04-03 21:34:48 -0700

    completed

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CreatePost.css'
+import { supabase } from '../client';
 
 const CreatePost = () => {
+    const [post, setPost] = useState({title: "", author: "", description: ""})
+    
+    const onValueChange = (e) => {
+        const {name, value} = e.target
+        setPost( (prev) => {
+            return {
+                ...prev,
+                [name]:value,
+            }
+        })
+        console.log(post)
+    }
+
+    const createPost = async(event) => {
+        event.preventDefault()
+        await supabase
+            .from('Posts')
+            .insert({title: post.title, author: post.title, description: post.description})
+            .select()
+
+        window.location = "/"
+
+    }
 
     return (
         <div>
             <form>
                 <label for="title">Title</label> <br />
-                <input type="text" id="title" name="title" /><br />
+                <input type="text" id="title" name="title"  value ={post.title} onChange={onValueChange}/><br />
                 <br/>
 
                 <label for="author">Author</label><br />
-                <input type="text" id="author" name="author" /><br />
+                <input type="text" id="author" name="author" value ={post.author} onChange={onValueChange}/><br />
                 <br/>
 
                 <label for="description">Description</label><br />
-                <textarea rows="5" cols="50" id="description">
+                <textarea rows="5" cols="50" id="description" name="description"  value ={post.description} onChange={onValueChange}>
                 </textarea>
                 <br/>
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" onClick={createPost}/>
             </form>
         </div>
     )
--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -1,29 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './EditPost.css'
+import { supabase } from '../client';
 
 const EditPost = ({data}) => {
-
     const {id} = useParams();
-    const post = data.filter(item => item.id === id)[0];
+    const [post, setPost] = useState({id: null, title: "", author: "", description: ""});
 
+    useEffect(() => {
+        setPost(data.filter(item => item.id == id)[0])
+    }, [])
+
+    const onValueChange = (e) => {
+        const {name, value} = e.target
+        setPost( (prev) => {
+            return {
+                ...prev,
+                [name]:value,
+            }
+        })
+        console.log(post)
+    }
+
+    const updatePost = async (event) =>  {
+        event.preventDefault();
+        const { error } = await supabase
+        .from('Posts')
+        .update({ title: post.title, author: post.author,  description: post.description})
+        .eq('id', id)
+
+        if (error) {
+            console.log(error);
+        }
+
+        window.location = "/";
+    }
+
+    const deletePost = async (event) => {
+        event.preventDefault();
+        const { error } = await supabase
+        .from('Posts')
+        .delete()
+        .eq('id', id) 
+
+        if (error) {
+            console.log(error);
+        }
+
+        window.location = "/";
+    }
     return (
         <div>
             <form>
+                
                 <label for="title">Title</label> <br />
-                <input type="text" id="title" name="title" value={post.title} /><br />
+                <input type="text" id="title" name="title" value={post.title} onChange={onValueChange}/><br />
                 <br/>
 
                 <label for="author">Author</label><br />
-                <input type="text" id="author" name="author" value={post.author} /><br />
+                <input type="text" id="author" name="author" value={post.author} onChange={onValueChange}/><br />
                 <br/>
 
                 <label for="description">Description</label><br />
-                <textarea rows="5" cols="50" id="description" value={post.description} >
+                <textarea rows="5" cols="50" name="description" id="description" value={post.description} onChange={onValueChange}>
                 </textarea>
                 <br/>
-                <input type="submit" value="Submit" />
-                <button className="deleteButton">Delete</button>
+                <input type="submit" value="Submit" onClick={updatePost}/>
+                <button className="deleteButton" onClick={deletePost}>Delete</button>
             </form>
         </div>
     )
